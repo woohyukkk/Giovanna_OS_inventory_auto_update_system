bbbcbf5b9957e84aba334ab46353c449b4e52b40
Chen He <woo328968014@gmail.com>
2018-05-11 13:22:55 -0400

    first commit
    first commit, Connected to local DB, read csv.

---
+++ OS_Inventory_auto_Reset_tool.py
@@ -0,0 +1,67 @@
+#Only work for Python 2.7, use cmd "py -2 xxxx.py" to run
+#version 0.01 Connect ATS_db and search for 0 styles returns
+import os
+import csv
+import smtplib
+import sys
+import mysql.connector
+from mysql.connector import errorcode
+
+##############################Conncet DB###############################
+config = {
+  'user': 'Chen He',
+  'password': '123456',
+  'host': 'localhost',
+  'database': 'winfa_ats',
+  'raise_on_warnings': True,
+}
+try:
+   cnx = mysql.connector.connect(**config)
+   print ('DataBase: '+config['database']+' connected.')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+
+  cnx.close()
+
+
+##############################Conncet DB###############################
+
+##############################Search csv###############################
+f= open('0 Styles_Reset.csv',"r")  
+look=csv.reader(f)
+style=''
+color=''
+size=''
+first=1
+n1=0;
+for item in look:
+    if first == 1:
+       first=0
+       continue
+    SKU=(item[1])
+    onhand=(item[2])
+    onhand=0
+    SKU.upper()
+    SKU=SKU.split('-')
+    if(len(SKU)==1):
+       SKU=SKU[0].split(' ')
+	   
+    #print (SKU)
+    style=SKU[0]
+    if len(SKU)>1:
+       color=SKU[1]
+    else:
+       color='N/A'
+    if len(SKU)>2: 
+       size =SKU[2]
+    else:
+       size='N/A'
+    print ("Style:"+style+"     Color:"+color+"     Size:"+size)
+
+
+
